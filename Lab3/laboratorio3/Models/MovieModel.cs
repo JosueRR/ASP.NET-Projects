d19de2468140412358c3918a303c4a8c29b53392
Josué R.R <73318183+JosueRR@users.noreply.github.com>
2023-04-11 17:35:48 -0600

    Crea un modelo

---
+++ Lab3/laboratorio3/Models/MovieModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace laboratorio3.Models
+{
+    public class MovieModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
