3f8e540af40ce8f2363284011981619e59078f66
Josué R.R <73318183+JosueRR@users.noreply.github.com>
2023-07-06 15:14:54 -0600

    Crea proyecto inicial y agrega instrucciones del examen

---
+++ Exam2/app_source/Controllers/PeliculasController.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using laboratorio6.Handlers;
+using laboratorio6.Models;
+
+namespace laboratorio6.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        public IActionResult Index()
+        {
+            PeliculasHandler peliculasHandler = new PeliculasHandler();
+            var peliculas = peliculasHandler.ObtenerPeliculas();
+            ViewBag.MainTitle = "Lista de Peliculas";
+            return View(peliculas);
+        }
+
+        [HttpGet]
+        public ActionResult CrearPelicula()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CrearPelicula(PeliculaModelo pelicula) 
+        {
+            ViewBag.ExitoAlCrear = false;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    PeliculasHandler peliculasHandler = new PeliculasHandler();
+                    ViewBag.ExitoAlCrear = peliculasHandler.CrearPelicula(pelicula);
+
+                    if (ViewBag.ExitoAlCrear)
+                    {
+                        ViewBag.Message = "La pelicula " + pelicula.Nombre + " fue creada con éxito. ";
+                        ModelState.Clear();
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "Algo salió mal y no se pudo crear la película";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult EditarPelicula(int? identificador)
+        {
+            ActionResult vista;
+            try
+            {
+                var peliculasHandler = new PeliculasHandler();
+                var pelicula = peliculasHandler.ObtenerPeliculas().Find(model => model.Id == identificador);
+                if (pelicula == null)
+                {
+                    vista = RedirectToAction("Index");
+                }
+                else
+                {
+                    vista = View(pelicula);
+                }
+            }
+            catch
+            {
+                vista = RedirectToAction("Index");
+            }
+            return vista;
+        }
+
+        [HttpPost]
+        public ActionResult EditarPelicula(PeliculaModelo pelicula)
+        {
+            try
+            {
+                var peliculasHandler = new PeliculasHandler();
+                peliculasHandler.EditarPelicula(pelicula);
+                return RedirectToAction("Index", "Peliculas");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult BorrarPelicula(int? identificador)
+        {
+            ActionResult vista;
+            try
+            {
+                var peliculasHandler = new PeliculasHandler();
+                var pelicula = peliculasHandler.ObtenerPeliculas().Find(model => model.Id == identificador);
+                if (pelicula == null)
+                {
+                    vista = RedirectToAction("Index");
+                }
+                else
+                {
+                    vista = View(pelicula);
+                }
+            }
+            catch
+            {
+                vista = RedirectToAction("Index");
+            }
+            return vista;
+        }
+
+        [HttpPost]
+        public ActionResult BorrarPelicula(PeliculaModelo pelicula)
+        {
+            try
+            {
+                var peliculasHandler = new PeliculasHandler();
+                peliculasHandler.BorrarPelicula(pelicula);
+                return RedirectToAction("Index", "Peliculas");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Exam2/app_source/Handlers/PeliculasHandlers.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using laboratorio6.Models;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+
+namespace laboratorio6.Handlers
+{
+    public class PeliculasHandler
+    {
+        private SqlConnection conexion;
+        private string rutaConexion;
+        public PeliculasHandler()
+        {
+            var builder = WebApplication.CreateBuilder();
+            rutaConexion =
+            builder.Configuration.GetConnectionString("ContextoDePeliculas");
+            conexion = new SqlConnection(rutaConexion);
+        }
+        private DataTable CrearTablaConsulta(string consulta)
+        {
+            SqlCommand comandoParaConsulta = new SqlCommand(consulta,
+            conexion);
+            SqlDataAdapter adaptadorParaTabla = new
+            SqlDataAdapter(comandoParaConsulta);
+            DataTable consultaFormatoTabla = new DataTable();
+            conexion.Open();
+            adaptadorParaTabla.Fill(consultaFormatoTabla);
+            conexion.Close();
+            return consultaFormatoTabla;
+        }
+        public List<PeliculaModelo> ObtenerPeliculas()
+        {
+            List<PeliculaModelo> peliculas = new List<PeliculaModelo>();
+            string consulta = "SELECT * FROM Pelicula";
+            DataTable tablaResultado = CrearTablaConsulta(consulta);
+            foreach (DataRow columna in tablaResultado.Rows)
+            {
+                peliculas.Add(
+                new PeliculaModelo
+                {
+                    Id = Convert.ToInt32(columna["Id"]),
+                    Nombre = Convert.ToString(columna["Nombre"]),
+                    Año = Convert.ToInt32(columna["Año"]),
+                });
+            }
+            return peliculas;
+        }
+
+        public bool CrearPelicula(PeliculaModelo pelicula)
+        {
+            var consulta = @"INSERT INTO [dbo].[Pelicula] ([Nombre],[Año]) VALUES(@Nombre, @Año) ";
+            var comandoParaConsulta = new SqlCommand(consulta, conexion);
+            comandoParaConsulta.Parameters.AddWithValue("@Nombre", pelicula.Nombre);
+            comandoParaConsulta.Parameters.AddWithValue("@Año", pelicula.Año);
+
+            conexion.Open();
+            bool exito = comandoParaConsulta.ExecuteNonQuery() >= 1;
+            conexion.Close();
+
+            return exito;
+        }
+
+        public bool EditarPelicula(PeliculaModelo pelicula)
+        {
+            var consulta = @"UPDATE [dbo].[Pelicula] SET
+                    Nombre = @Nombre,
+                    Año = @Año
+                    WHERE Id=@Id";
+
+            var cmdParaConsulta = new SqlCommand(consulta, conexion);
+            cmdParaConsulta.Parameters.AddWithValue("@Nombre", pelicula.Nombre);
+            cmdParaConsulta.Parameters.AddWithValue("@Año", pelicula.Año);
+            cmdParaConsulta.Parameters.AddWithValue("@Id", pelicula.Id);
+
+            conexion.Open();
+            bool exito = cmdParaConsulta.ExecuteNonQuery() >= 1;
+            conexion.Close();
+
+            return exito;
+        }
+
+        public bool BorrarPelicula(PeliculaModelo pelicula)
+        {
+            var consulta = @"DELETE [dbo].[Pelicula] WHERE Id=@Id";
+
+            var cmdParaConsulta = new SqlCommand(consulta, conexion);
+            cmdParaConsulta.Parameters.AddWithValue("@Id", pelicula.Id);
+
+            conexion.Open();
+            bool exito = cmdParaConsulta.ExecuteNonQuery() >= 1;
+            conexion.Close();
+
+            return exito;
+        }
+    }
+}
