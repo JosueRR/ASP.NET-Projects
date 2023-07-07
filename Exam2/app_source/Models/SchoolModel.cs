938ce58308ac7f2c90063920a7f637eb6dfe9b0d
Josué R.R <73318183+JosueRR@users.noreply.github.com>
2023-07-07 00:00:30 -0600

    Genera modelo de las escuelas

---
+++ Exam2/app_source/Models/SchoolModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace app.Models
+{
+    public class SchoolModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar un nombre")]
+        [DisplayName("Nombre de la escuela")]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar un Provincia")]
+        [DisplayName("Provincia de la escuela")]
+        public string? Provincia { get; set;}
+
+        [Required(ErrorMessage = "Debe ingresar un Estado")]
+        [DisplayName("Estado o distrito de la escuela")]
+        public string? Estado { get; set;}
+
+        [Required(ErrorMessage = "Debe ingresar un NumeroAulas")]
+        [DisplayName("Cantidad de Aulas de la escuela")]
+        public int? NumeroAulas { get; set;}
+
+        [Required(ErrorMessage = "Debe ingresar un EsPublica")]
+        [DisplayName("Es publica la escuela?")]
+        public bool? EsPublica { get; set;}
+    }
+}
